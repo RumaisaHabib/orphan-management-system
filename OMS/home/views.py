b4c19b8b3c9792fc05c6a3722bfad3846878776d
DanyalMaq <danyalmaqbool@hotmail.com>
2021-11-07 21:26:58 +0500

    Fecthed first data

--- OMS/home/views.py
+++ OMS/home/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Temporary")
+        print(cursor.fetchall())
     return render(request, "home/index.html")
