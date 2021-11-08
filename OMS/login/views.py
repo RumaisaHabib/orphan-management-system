c859c395b9674632ff4da96e1907e834f7e59380
Rumi <rumaisahabib999@gmail.com>
2021-11-08 17:45:27 +0500

    Signup adds to database

--- OMS/login/views.py
+++ OMS/login/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.db import connection
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        print(request.POST)
+        email = request.POST["email"]
+        print(email)
+        pwd = request.POST["password"]
+        sql = fr"INSERT INTO Users ('Email', 'Password') VALUES ('{email}', '{pwd}');"
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+        return render(request, "login/login.html")
     return render(request, "login/login.html")
--- OMS/signup/views.py
+++ OMS/signup/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.db import connection
 
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        print(request.POST)
+        email = request.POST["email"]
+        print(email)
+        pwd = request.POST["password"]
+        sql = fr"INSERT INTO Users ('Email', 'Password') VALUES ('{email}', '{pwd}');"
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+        return render(request, "signup/signup.html")
     return render(request, "signup/signup.html")
+    
