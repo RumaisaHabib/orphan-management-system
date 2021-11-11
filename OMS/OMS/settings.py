fcf407acdfef6b7ecef81740dcb684a77aa1870a
Hisan Naeem <hsn37@outlook.com>
2021-11-11 17:32:42 +0500

    Changed db to mysql

--- OMS/OMS/settings.py
+++ OMS/OMS/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'OMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oms',
+        'USER': 'deebee',
+        'HOST': 'physlogger.com',
+        'PASSWORD': 'Deeb33@us'
     }
 }
 
