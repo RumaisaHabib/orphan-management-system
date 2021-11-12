5d9db95795877cd696715940506f3016369ad72d
Hisan Naeem <hsn37@outlook.com>
2021-11-12 16:27:40 +0500

    added a helper function for convenience

--- OMS/helpers/format.py
+++ OMS/helpers/format.py
@@ -1,3 +1,5 @@
+from django.db import connection
+
 def format_query(data, attributes): 
     '''
     Returns a list of dictionaries of the given query set for ease of accessing data.
@@ -13,4 +15,29 @@ def format_query(data, attributes):
     result = []
     for row in data:
         result.append(dict(zip(attributes, row)))
+    return result
+
+
+def executeSQL(sqlcom, colnames=[]):
+    '''
+    Returns a list of dictionaries of the given query set for ease of accessing data.
+    
+    params:
+    @data
+    This is the raw query set we get after an SQL query.
+
+    @attributes
+    These are the attributes we want to map the data onto. It's important that the order
+    of attributes matches how the data is represented in the databse.
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute(sqlcom)
+        data = cursor.fetchall()
+
+    if len(colnames) == 0:
+        colnames = ['col' + str(x + 1) for x in range(len(data[0]))]
+
+    result = []
+    for row in data:
+        result.append(dict(zip(colnames, row)))
     return result
