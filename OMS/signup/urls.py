ed7116c179027079bd8fa60d099e01a1381d8663
Rumi <rumaisahabib999@gmail.com>
2021-11-24 18:46:09 +0500

    Has error in adding user to database

--- OMS/signup/urls.py
+++ OMS/signup/urls.py
@@ -6,6 +6,6 @@ app_name = 'signup'
 
 urlpatterns = [
     path('', views.signup, name="signup"),
-    path('signupvolunteer/', views.signupVolunteer, name='signupVolunteer'),
-    path('signupparent/', views.signupParent, name='signupParent'),  
+    path('volunteer/', views.signupVolunteer, name='signupVolunteer'),
+    path('parent/', views.signupParent, name='signupParent'),  
 ]
--- OMS/signup/views.py
+++ OMS/signup/views.py
@@ -11,7 +11,7 @@ def signupVolunteer(request):
         print(email)
         pwd = request.POST["password"]
         hashed_pwd = str(int(hashlib.sha256(pwd.encode('utf-8')).hexdigest(), 16) % 10**8)
-        utype = request.POST["usertype"]
+        utype = "volunteer"
         sql = fr"INSERT INTO Users (Email, Password, Usertype) VALUES('{email}', '{hashed_pwd}', '{utype}');"
         with connection.cursor() as cursor:
             cursor.execute(sql)
@@ -29,4 +29,22 @@ def signup(request):
     return render(request, "signup/signup.html")
 
 def signupParent(request):
-    pass
+    if request.method == "POST":
+        print(request.POST)
+        email = request.POST["email"]
+        print(email)
+        pwd = request.POST["password"]
+        hashed_pwd = str(int(hashlib.sha256(pwd.encode('utf-8')).hexdigest(), 16) % 10**8)
+        utype = "parent"
+        sql = fr"INSERT INTO Users (Email, Password, Usertype) VALUES('{email}', '{hashed_pwd}', '{utype}');"
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            request.session['logged_in'] = 1
+            request.session['usertype'] = utype
+        return redirect('home:index')
+    else:
+        if request.session.get("logged_in")==1:
+            print("you're already logged in")
+            # another page rendered instead: to be made.
+            return redirect('home:index')
+        return render(request, "signup/signup.html")
